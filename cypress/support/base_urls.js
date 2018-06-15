d9713c86b2eaa9b7af79ebc1c53bcbe829d898a1
sapar <sapar@meyvndigital.co.uk>
2018-06-15 20:18:54 +0300

    delete

--- cypress/support/base_urls.js
+++
@@ -1,8 +0,0 @@
-import config from './config'
-
-export default {
-  getBaseUrl: (subdomain) => {
-    subdomain = subdomain ? subdomain : config.DEFAULT_SUBDOMAIN
-    return `${config.PROTOCOL}://${subdomain}.${config.HOST}`
-  }
-}
--- cypress/support/config.js
+++
@@ -1,7 +0,0 @@
-const conf = {
-  PROTOCOL: 'http',
-  DEFAULT_SUBDOMAIN: 'www',
-  HOST: 'lvh.me:30001'
-}
-
-export default Object.freeze(conf)
